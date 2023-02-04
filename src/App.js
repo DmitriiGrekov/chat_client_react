7f1bc99b8401d5b550271bcfc5f08c8b253e6b11
DmitriiGrekov <grekovdima7@gmail.com>
2023-02-04 18:31:36 +0300

    *

--- src/App.js
+++ src/App.js
@@ -1,51 +1,89 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Component, useEffect } from 'react';
-import { MessageList } from './components/Message/MessageList';
-import { useDispatch, useSelector } from 'react-redux';
-import { RoomList } from '../src/components/Rooms/RoomsList';
-import { MessageInput } from '../src/components/Message/MessageInput';
-import { ModalComponent } from './components/Modal/ModalComponent';
-import { CheckUserLogin } from './components/Common/CheckUserLogin';
-import { redirect, useNavigate } from 'react-router-dom';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component, useEffect } from "react";
+import { MessageList } from "./components/Message/MessageList";
+import { useDispatch, useSelector } from "react-redux";
+import { RoomList } from "../src/components/Rooms/RoomsList";
+import { MessageInput } from "../src/components/Message/MessageInput";
+import { ModalComponent } from "./components/Modal/ModalComponent";
+import { CheckUserLogin } from "./components/Common/CheckUserLogin";
+import { redirect, useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+import axios from "axios";
 
 function App() {
-
   const navigate = useNavigate();
 
   const handleExit = (e) => {
-    localStorage.setItem('token', '')
-    navigate('/')
-  }
+    localStorage.setItem("token", "");
+    navigate("/");
+  };
 
-  const token = localStorage.getItem('token');
-  if (!token)
-    navigate('/')
+  const token = localStorage.getItem("token");
+  if (!token) navigate("/");
+
+  const dispatch = useDispatch();
+  const rooms = useSelector((state) => state.data);
+  const currentRoomId = useSelector((state) => state.roomId);
+  let currentRoomName = "";
+  if (currentRoomId)
+    currentRoomName =
+      rooms.find((room) => room.id === currentRoomId).name ?? "";
+
+  useEffect(() => {
+    let socket = io(`${process.env.REACT_APP_SOCKET_URL}/chat`, {
+      extraHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    socket.on("connect", function() {
+      console.log('connect site')
+    });
+    socket.on("chatsUnreadedMessage", function(data) {
+      console.log(data)
+      axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/rooms/list`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          dispatch({ type: "LOAD_ROOM", payload: res.data });
+        });
+    });
+
+    socket.on("disconnect", function() {
+      console.log('disconnect site')
+    });
+    return () => {
+      socket.disconnect(true);
+    };
+  }, [rooms, token, dispatch]);
 
   return (
-    <div class='container'>
-      <div class='row'>
-        <div class='col-xl-4'>
-
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-4">
           <div class="container mt-3 rooms">
             <ModalComponent />
-            <span class='' style={{ marginLeft: '10px' }}>{localStorage.getItem('userLogin')} <a class='btn btn-secondary' onClick={handleExit}>Выйти</a></span>
+            <span class="" style={{ marginLeft: "10px" }}>
+              {localStorage.getItem("userLogin")}{" "}
+              <a class="btn btn-secondary" onClick={handleExit}>
+                Выйти
+              </a>
+            </span>
             <RoomList />
           </div>
         </div>
 
-        <div class='col-xl-8'>
-          <div class='chat'>
-
+        <div class="col-xl-8">
+          <h5 class="mt-3">{currentRoomName}</h5>
+          <div class="chat">
             <MessageList />
-          </div >
+          </div>
           <MessageInput />
-        </div >
-
-
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,60 +1,109 @@
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import { redirect, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { redirect, useNavigate } from "react-router-dom";
 
 export function Register(props) {
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const errorLoginMessage = useSelector(state => state.errorLoginMessage);
-  const errorLoginMessageVisible = useSelector(state => state.errorLoginMessageVisible);
+  const errorLoginMessage = useSelector((state) => state.errorLoginMessage);
+  const errorLoginMessageVisible = useSelector(
+    (state) => state.errorLoginMessageVisible
+  );
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
     const target = e.target;
-    const form = new FormData(target)
+    const form = new FormData(target);
     const data = {
-      phone: form.get('phone'),
-      email: form.get('email'),
-      password: form.get('password'),
-      login: form.get('login')
-    }
-    axios.post(`${process.env.REACT_APP_SERVER_URL}/users`, data).then(res => {
-      localStorage.setItem('token', res.data.access_token)
-      localStorage.setItem('userId', res.data.user_id)
-      // return redirect('/main')
-      console.log(res.data);
-      navigate('/');
-    }).catch((e) => {
-      dispatch({ type: "SET_ERROR_MESSAGE_LOGIN", payload: { errorLoginMessage: e.response.data.message, errorLoginMessageVisible: 'block' } })
-    })
-  }
+      phone: form.get("phone"),
+      email: form.get("email"),
+      password: form.get("password"),
+      login: form.get("login"),
+    };
+    axios
+      .post(`${process.env.REACT_APP_SERVER_URL}/users`, data)
+      .then((res) => {
+        localStorage.setItem("token", res.data.access_token);
+        localStorage.setItem("userId", res.data.user_id);
+        // return redirect('/main')
+        navigate("/");
+      })
+      .catch((e) => {
+        dispatch({
+          type: "SET_ERROR_MESSAGE_LOGIN",
+          payload: {
+            errorLoginMessage: e.response.data.message,
+            errorLoginMessageVisible: "block",
+          },
+        });
+      });
+  };
 
   return (
-    < div class='container' >
+    <div class="container">
       <form onSubmit={handleOnSubmit}>
-        <div class="alert alert-danger mt-3" style={{ display: errorLoginMessageVisible }} role="alert" >
+        <div
+          class="alert alert-danger mt-3"
+          style={{ display: errorLoginMessageVisible }}
+          role="alert"
+        >
           {errorLoginMessage}
         </div>
         <div class="mb-3 mt-3">
-          <label for="phone" class="form-label">Телефон</label>
-          <input type="text" class="form-control" id="phone" name='phone' placeholder='Телефон' aria-describedby="emailHelp" />
+          <label for="phone" class="form-label">
+            Телефон
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="phone"
+            name="phone"
+            placeholder="Телефон"
+            aria-describedby="emailHelp"
+          />
         </div>
         <div class="mb-3">
-          <label for="email" class="form-label">Почта</label>
-          <input type="email" class="form-control" id="email" name='email' placeholder='Почта' aria-describedby="emailHelp" />
+          <label for="email" class="form-label">
+            Почта
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            name="email"
+            placeholder="Почта"
+            aria-describedby="emailHelp"
+          />
         </div>
         <div class="mb-3">
-          <label for="login" class="form-label">Логин</label>
-          <input type="text" class="form-control" id="login" name='login' placeholder='Логин' aria-describedby="emailHelp" />
+          <label for="login" class="form-label">
+            Логин
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="login"
+            name="login"
+            placeholder="Логин"
+            aria-describedby="emailHelp"
+          />
         </div>
         <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Пароль</label>
-          <input type="password" class="form-control" name='password' placeholder='Пароль' id="exampleInputPassword1" />
+          <label for="exampleInputPassword1" class="form-label">
+            Пароль
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            name="password"
+            placeholder="Пароль"
+            id="exampleInputPassword1"
+          />
         </div>
-        <button type="submit" class="btn btn-primary">Зарегестрироваться</button>
+        <button type="submit" class="btn btn-primary">
+          Зарегестрироваться
+        </button>
       </form>
-    </div >
-
-  )
+    </div>
+  );
 }
